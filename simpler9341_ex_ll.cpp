1a8ab9bcc678b1980005e6f8dd75cd65802e7a4c
mean <fixounet@free.fr>
2022-01-26 07:41:02 +0100

    pretemplate

--- simpler9341_ex_ll.cpp
+++ simpler9341_ex_ll.cpp
@@ -1,10 +1,4 @@
 #include "simpler9341.h"
-#ifdef ENABLE_FONT_COMPRESSION 
-extern "C"
-{
-    #include "heatshrink_decoder.h"
-}
-#endif
 /**
  * 
  * @param x
@@ -16,51 +10,6 @@ extern "C"
  * @return 
  */
 #define debug(...) {}
-
-void ili9341::innerLoop1Nc(int w, int h, int left, int lineSize,int color, int bg,uint8_t *p)
-{
-    int  bits = 0, bit = 0;
-    uint8_t *col;
-    for( int line=h-1;line>=0;line--)
-    {      
-        col=_column+left*2;     
-        // mid
-        for( int xcol=w-1;xcol>=0;xcol--)
-        {
-            if(!bit) // reload ?
-            {
-                bits= *p++;
-                if(xcol>=8) // speed up some special cases
-                {
-                    uint8_t *table8=(uint8_t *)_lookup;
-                    uint8_t *high=table8+(bits>>4)*4;
-                    uint8_t *low=table8+(bits&0xf)*4;                
-                    col[0]=high[0];
-                    col[1]=high[1];
-                    col[2]=high[2];
-                    col[3]=high[3];
-                    col[4]=low[0];
-                    col[5]=low[1];
-                    col[6]=low[2];
-                    col[7]=low[3];
-
-                    //memcpy(col,high,4);
-                    //memcpy(col+4,low,4);
-                    xcol-=7;
-                    bit=0;
-                    col+=8;
-                    continue;
-                }
-                bit = 0x80;
-            }      
-                            
-            *col++=!!(bits & bit) ;  
-            bit=bit>>1;
-        }
-        // 9ms here
-        push2Colors(_column,lineSize,color,bg);
-    }   
-}
 
 /**
  * 
--- simpler9341_ex_ll_1fc.cpp
+++ simpler9341_ex_ll_1fc.cpp
@@ -1,8 +1,5 @@
 #include "simpler9341.h"
-extern "C"
-{
-    #include "heatshrink_decoder.h"
-}
+#include "simpler9341_ex_hs.h"
 /**
  * 
  * @param w
@@ -15,13 +12,7 @@ extern "C"
  */
 void ili9341::innerLoop1C(int w, int h, int left, int lineSize,int color, int bg,uint8_t *p)
 {
-    // Initialize heatshrink...
-    heatshrink_decoder *hsd = heatshrink_decoder_alloc(64,8,4); // same as flatconvert!
-    uint8_t decompBuffer[64];
-    uint8_t *tail;
-    uint8_t *head;
-    
-    tail=head=decompBuffer;
+    iliHS hs(p);
     
     int  bits = 0, bit = 0;
     uint8_t *col;
@@ -29,30 +20,13 @@ void ili9341::innerLoop1C(int w, int h, int left, int lineSize,int color, int bg
     {      
         col=_column+left;     
         // mid
-        size_t count;
         for( int xcol=w-1;xcol>=0;xcol--)
         {
             
             if(!bit) // reload ?
             {
-                if(head==tail)
-                { 
-                    again:
-                   // Data available ?
-                   int r=heatshrink_decoder_poll(hsd, decompBuffer,64,&count);
-                   if(r==HSDR_POLL_EMPTY && count==0)
-                   {
-                        
-                        int r=heatshrink_decoder_sink(hsd, p, 64*1024, &count) ;
-                        xAssert(r>=0);
-                        p+=count;
-                        goto again;
-                   }
-                   head=decompBuffer;
-                   tail=head+count;
-                    
-                } // nest
-                bits= *head++;
+       
+                bits= hs.next();
                 if(xcol>=8) // speed up some special cases
                 {
                     uint8_t *table8=(uint8_t *)_lookup;
@@ -80,6 +54,5 @@ void ili9341::innerLoop1C(int w, int h, int left, int lineSize,int color, int bg
         // 9ms here
         push2Colors(_column,lineSize,color,bg);
     }  
-    heatshrink_decoder_free(hsd);
 }
 // EOF
