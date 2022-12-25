7674303819b9e2a64650b90dc613fb25a54fb372
mean <fixounet@free.fr>
2022-12-25 08:34:03 +0100

    add non heatshrunk bitmap

--- rust/ili9341/src/ili9341/bitmap.rs
+++ rust/ili9341/src/ili9341/bitmap.rs
@@ -60,4 +60,63 @@ impl<'a> Ili9341<'a> {
     }
     self.access.data_end();
     }
+
+    //-------------------------------------
+    pub fn draw_bitmap(&mut self, width : usize, height: usize, wx: usize, wy: usize, fg: u16, bg : u16, bitmapdata : &'a [u8])
+    {
+    
+    self.access.set_address(wx, wy,  width, height);
+
+    let nb_pixels =width * height;
+    let mut pixel: usize = 0;        
+    let mut cur : usize;
+    let mut ready : usize=0;    
+    let mut color : u16;    
+    let  output_slice : &mut [u16];
+    let mut output_index : usize = 0;
+    unsafe {
+    output_slice = core::slice::from_raw_parts_mut( self.src_buf,ST7735_BUFFER_SIZE_WORD);
+    }
+    self.access.data_begin();
+    let fg = self.access.color_map(fg);
+    let bg = self.access.color_map(bg);
+    let mut ix = 0;
+    while pixel<nb_pixels
+    {
+        // load next
+        cur = bitmapdata[ix] as usize;ix+=1;        
+        let mut mask=0x80;
+        for _i in 0..8
+        {
+            if (mask & cur)!=0
+            {
+                color=fg;
+            }else
+            {
+                color=bg; //color=0xff00*0+1*bg;
+            }
+            mask>>=1;
+            output_slice[output_index]=color;
+            output_index+=1;
+            ready+=1;
+        }
+        if ready>ST7735_BUFFER_SIZE_WORD-16
+        { // Flush
+            unsafe  {
+                self.access.send_words( core::slice::from_raw_parts(self.src_buf, ready));
+            }
+            ready=0;
+            output_index = 0;
+        }
+        
+        pixel+=8;
+    }
+    if ready>0
+    {
+        unsafe{
+        self.access.send_words( core::slice::from_raw_parts(self.src_buf, ready) );
+        }
+    }
+    self.access.data_end();
+    }
 }
